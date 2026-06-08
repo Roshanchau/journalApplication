a0d160e300232c9a32c7b55a0cb25952cbc88db0
Roshanchau <roshanchau001@gmail.com>
2026-06-08 14:45:08 +0545

    spring secruity

---
+++ src/main/java/com/example/journalApplication/config/SpringSecurity.java
@@ -0,0 +1,28 @@
+package com.example.journalApplication.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurity {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception{
+        http
+                .authorizeHttpRequests(auth-> auth
+                        .requestMatchers("/journal/**").authenticated()
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(httpBasic-> {})
+                .csrf(csrf-> csrf.disable());
+
+        return http.build();
+    }
+
+}
